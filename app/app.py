e7a115682b278adfe81061394ebda7410e906892
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2021-09-23 18:50:35 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,9 +8,22 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+def get_prediction():
+    image = request.files.get("imagefile")
+    image_np = cv2.imread(image,-1)
+    image_np = image_np.reshape((1,512,512,3))
+    model = load_model('satellitesegment.h5')
+    result_img = model.predict(image_np)
+    result_img = result_img[:,:,:,:]>0.5
+    result_img = result_img[0,:,:,0]
+    result_img = Image.fromarray(result_img)
+    return result_img
+    
+
+
 @app.route("/predict",methods=["GET","POST"])
 def predict():
-    prediction = Prediction.get_prediction()
+    prediction = get_prediction()
     return render_template("predict.html",prediction=prediction)
 
 
